dbf6296d4dac562d7ea491a966afed17016f4df7
kazan <kazanceve.gor@gmail.com>
2025-02-16 18:05:30 +0700

    fix token

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    token: '6600094203:AAEWGt9GWvkAkgAKIkEP3A5xP3vQzNqrON0',
+    // token: '6600094203:AAEWGt9GWvkAkgAKIkEP3A5xP3vQzNqrON0', dev
+    token: '7616340811:AAHgfFXZcE2adjorf9dOujFDOP5HixEHjss',
     urlWeb: 'https://arm-dom.tomsk.ru/'
 }

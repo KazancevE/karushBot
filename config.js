9cebce99079bb2c152dcfa605f74301c43332223
KazancevE <kazanceve.gor@gmail.com>
2025-04-08 23:37:44 +0700

    delete config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // token: '6600094203:AAEWGt9GWvkAkgAKIkEP3A5xP3vQzNqrON0', //dev
-    token: '7616340811:AAEUdzFXCQmcQTipXggK1ExZ72S1dnXJFac',
-    urlWeb: 'https://%D0%95%D0%BB%D0%B8%D0%B4%D0%B0%D0%B5%D0%BB%D0%B8.su'
-}

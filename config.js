0613df12e1b9226f16faf1943dccf69613f977a4
KazancevE <kazanceve.gor@gmail.com>
2025-04-08 15:05:32 +0700

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '6600094203:AAEWGt9GWvkAkgAKIkEP3A5xP3vQzNqrON0', //dev
-    // token: '7616340811:AAHgfFXZcE2adjorf9dOujFDOP5HixEHjss',
+    // token: '6600094203:AAEWGt9GWvkAkgAKIkEP3A5xP3vQzNqrON0', //dev
+    token: '7616340811:AAHgfFXZcE2adjorf9dOujFDOP5HixEHjss',
     urlWeb: 'https://%D0%95%D0%BB%D0%B8%D0%B4%D0%B0%D0%B5%D0%BB%D0%B8.su'
 }
